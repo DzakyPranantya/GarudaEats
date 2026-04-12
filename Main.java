0424b1a4c0cc5418d9197dcb6ce51d44d576ef49
Dzaky Pranantya <129468246+DzakyPranantya@users.noreply.github.com>
2026-04-12 21:15:12 +0700

    Fixed

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ public class Main {
 
         Kantin kantin = new Kantin("GarudaEats");
 
-        MenuItem m1 = new MenuItem("Nasi Goreng", 15000, "Makanan", 5);
-        MenuItem m2 = new MenuItem("Mie Ayam", 12000, "Makanan", 5);
+        MenuItem m1 = new MenuItem("Nasi Pancal", 3000, "Makanan", 5);
+        MenuItem m2 = new MenuItem("Bakso Pentol Goreng", 15000, "Makanan", 5);
         MenuItem m3 = new MenuItem("Es Teh", 5000, "Minuman", 5);
-        MenuItem m4 = new MenuItem("Kopi", 8000, "Minuman", 5);
-        MenuItem m5 = new MenuItem("Keripik", 3000, "Snack", 5);
-        MenuItem m6 = new MenuItem("Coklat", 7000, "Snack", 5);
+        MenuItem m4 = new MenuItem("Kopi Dukun", 8000, "Minuman", 5);
+        MenuItem m5 = new MenuItem("Weci", 3000, "Snack", 5);
+        MenuItem m6 = new MenuItem("Hongkong", 3000, "Snack", 5);
 
         kantin.tambahMenu(m1);
         kantin.tambahMenu(m2);
@@ -17,8 +17,8 @@ public class Main {
         kantin.tambahMenu(m5);
         kantin.tambahMenu(m6);
 
-        Customer c1 = new Customer("Ujang", "001");
-        Customer c2 = new Customer("Dudung", "002", 10000);
+        Customer c1 = new Customer("Dzaky", "102022530077");
+        Customer c2 = new Customer("Naufal", "102022500384", 10000);
 
         c1.topUpSaldo(50000);
 
@@ -26,7 +26,7 @@ public class Main {
         kantin.tampilkanMenuTersedia();
 
         System.out.println("\nCARI:");
-        MenuItem cari = kantin.cariMenuByNama("Nasi Goreng");
+        MenuItem cari = kantin.cariMenuByNama("Nasi Pancal");
         if (cari != null) cari.displayInfo();
 
         System.out.println("\nORDER 1:");
